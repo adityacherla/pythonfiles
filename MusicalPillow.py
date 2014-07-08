017b77dec9a4ef4cd46a500bbd22546556bf78bf
adityacherla <reco.lightstar@gmail.com>
2014-07-08 11:29:24 +0530

    Update MusicalPillow.py
    changed a few comments

--- MusicalPillow.py
+++ MusicalPillow.py
@@ -1,10 +1,14 @@
 __author__='Aditya Cherla'
 
 """
-credits to jonrsharpe@stackoverflow for the main logic.
-I was writing a pretty messy code he showed the smart way. 
+credits to jonrsharpe@stackoverflow.com for the simple main logic.
+I was writing a pretty messy code, he showed the smart way
+do it. 
 
-This is an implementation of the musical pillow problem.
+This is an implementation of the musical chairs problem.
+You have certain number of people in the circle and the length of the song
+that will played. based on this parameters you have to find the one last 
+person remaining in the game.
 The getWinner method gives the last remaining person in the circle.
 You need to pass the number of the people 
 """
